1008354b3147980f8a411fab3b318f3ed6d66a78
Coder-Delta <ranjitbhandary15@gmail.com>
2025-11-26 11:39:33 +0530

    refactor user routes; organize imports and improve code formatting for better readability

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,30 +1,45 @@
 import { Router } from "express";
-import {registerUser,loginUser, logoutUser, refereshAccessToken} from "../controllers/user.controllers.js"
-import { upload } from "../middlewares/multer.middlewares.js"
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refereshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
-
-
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name : "avatar",
-            maxCount: 1
-        },
-        {
-            name : "coverImage",
-            maxCount : 1
-        }
-    ]),
-    registerUser
-)
-
-router.route("/login").post(loginUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)//verify is a middleware
-router.route("/refresh-token").post(refereshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser); //verify is a middleware
+router.route("/refresh-token").post(refereshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"));
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
-export default router //jasa name chaihe use name se import kar sakta hu agar default hai to
+export default router; //jasa name chaihe use name se import kar sakta hu agar default hai to
