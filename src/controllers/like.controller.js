0dc362c283feaf6d54f8829082c78764735882b2
Coder-Delta <ranjitbhandary15@gmail.com>
2025-12-06 08:24:26 +0530

    Complete like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,21 +5,129 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-})
+    const { videoId } = req.params;
+    const userId = req.user._id;
+
+    if (!videoId) {
+        throw new ApiError(400, "Video ID is required");
+    }
+
+    const existingLike = await Like.findOne({
+        video: videoId,
+        likedBy: userId,
+    });
+
+    if (existingLike) {
+        await Like.deleteOne({ _id: existingLike._id });
+
+        return res.status(200).json(
+            new ApiResponse(
+                200,
+                {},
+                "Video unliked successfully"
+            )
+        );
+    }
+
+    // Like the video
+    const newLike = await Like.create({
+        video: videoId,
+        likedBy: userId,
+    });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            newLike,
+            "Video liked successfully"
+        )
+    );
+});//complete
+
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    const userId = req.user._id;
 
-})
+    if (!commentId) {
+        throw new ApiError(400, "Comment ID is required");
+    }
+
+    const existingLike = await Like.findOne({
+        comment: commentId,
+        likedBy: userId,
+    });
+
+    if (existingLike) {
+        await Like.deleteOne({ _id: existingLike._id });
+
+        return res.status(200).json(
+            new ApiResponse(
+                200,
+                {},
+                "Comment unliked successfully"
+            )
+        );
+    }
+
+    // Like the video
+    const newLike = await Like.create({
+        comment: commentId,
+        likedBy: userId,
+    });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            newLike,
+            "Comment liked successfully"
+        )
+    );
+
+})//complete
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+
+    const userId = req.user._id;
+
+    if (!tweetId) {
+        throw new ApiError(400, "Tweet ID is required");
+    }
+
+    const existingLike = await Like.findOne({
+        tweet: tweetId,
+        likedBy: userId,
+    });
+
+    if (existingLike) {
+        await Like.deleteOne({ _id: existingLike._id });
+
+        return res.status(200).json(
+            new ApiResponse(
+                200,
+                {},
+                "Tweet unliked successfully"
+            )
+        );
+    }
+
+    // Like the video
+    const newLike = await Like.create({
+        tweet: tweetId,
+        likedBy: userId,
+    });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            newLike,
+            "Tweet liked successfully"
+        )
+    )
+})//complete
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
